47539968cf0f9030f15baad418082b1cd19ae989
ayerfph <jettayam01122@gmail.com>
2025-06-23 15:36:09 +0800

    Learning Django Ep-2: TODO List

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,11 @@ from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
     #print(tasks)
+
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('updated_at')
+    #print(completed_tasks)
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks
     }
     return render(request, 'home.html', context)
